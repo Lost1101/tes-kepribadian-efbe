13115e874833c64df11db8920210cc6147f27edc
Lost1101 <kamilah.insani60@gmail.com>
2024-11-11 20:51:02 +0700

    fixbug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Loading from "./components/Loading";
-import Result from "./components/Result";
+import { useState, useEffect } from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Loading from "./components/Loading"
+import Result from "./components/Result"
+import test from '../public/karbit.jpeg'
 
 export default function App() {
   const [hash, setHash] = useState("");
   const [showResult, setShowResult] = useState(false);
+  console.log(test)
 
   useEffect(() => {
     if (hash) {
